c66443b309d738bddc9cbca19cf8a31ae7b540d3
lilei <lilei931031@gmail.com>
2016-11-21 20:40:50 +0800

    Add listener

--- app/src/main/java/com/lilei/example/MainActivity.java
+++ app/src/main/java/com/lilei/example/MainActivity.java
@@ -3,8 +3,10 @@ package com.lilei.example;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.lilei.springactionmenu.ActionMenu;
+import com.lilei.springactionmenu.OnActionItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +23,27 @@ public class MainActivity extends AppCompatActivity {
         actionMenu.addView(R.drawable.search, getItemColor(R.color.menuNormalInfo), getItemColor(R.color.menuPressInfo));
         actionMenu.addView(R.drawable.like, getItemColor(R.color.menuNormalRed), getItemColor(R.color.menuPressRed));
         actionMenu.addView(R.drawable.write);
+
+
+        actionMenu.setItemClickListener(new OnActionItemClickListener() {
+            @Override
+            public void onItemClick(int index) {
+                showMessage("Click " + index);
+            }
+
+            @Override
+            public void onAnimationEnd(boolean isOpen) {
+                showMessage("animation end : " + isOpen);
+            }
+        });
     }
 
 
     private int getItemColor(int colorID) {
         return getResources().getColor(colorID);
+    }
+
+    private void showMessage(String content) {
+        Toast.makeText(this, content, Toast.LENGTH_SHORT).show();
     }
 }
