e85d31886bebb17f3e14e5865ebeab5e90e5b2c8
lilei <lilei931031@gmail.com>
2016-11-22 11:32:06 +0800

    update example

--- app/src/main/java/com/lilei/example/MainActivity.java
+++ app/src/main/java/com/lilei/example/MainActivity.java
@@ -10,22 +10,28 @@ import com.lilei.springactionmenu.OnActionItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActionMenu actionMenu;
+    private ActionMenu actionMenuTop;
+    private ActionMenu actionMenuBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        actionMenu = (ActionMenu) findViewById(R.id.actionMenu);
+        actionMenuTop = (ActionMenu) findViewById(R.id.actionMenuTop);
+        actionMenuBottom = (ActionMenu) findViewById(R.id.actionMenuBottom);
 
         // add menu items
-        actionMenu.addView(R.drawable.search, getItemColor(R.color.menuNormalInfo), getItemColor(R.color.menuPressInfo));
-        actionMenu.addView(R.drawable.like, getItemColor(R.color.menuNormalRed), getItemColor(R.color.menuPressRed));
-        actionMenu.addView(R.drawable.write);
+        actionMenuTop.addView(R.drawable.search, getItemColor(R.color.menuNormalInfo), getItemColor(R.color.menuPressInfo));
+        actionMenuTop.addView(R.drawable.like, getItemColor(R.color.menuNormalRed), getItemColor(R.color.menuPressRed));
+        actionMenuTop.addView(R.drawable.write);
+
+        actionMenuBottom.addView(R.drawable.search, getItemColor(R.color.menuNormalInfo), getItemColor(R.color.menuPressInfo));
+        actionMenuBottom.addView(R.drawable.like, getItemColor(R.color.menuNormalRed), getItemColor(R.color.menuPressRed));
+        actionMenuBottom.addView(R.drawable.write);
 
 
-        actionMenu.setItemClickListener(new OnActionItemClickListener() {
+        actionMenuBottom.setItemClickListener(new OnActionItemClickListener() {
             @Override
             public void onItemClick(int index) {
                 showMessage("Click " + index);
@@ -33,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(boolean isOpen) {
-                showMessage("animation end : " + isOpen);
+         //       showMessage("animation end : " + isOpen);
             }
         });
     }
